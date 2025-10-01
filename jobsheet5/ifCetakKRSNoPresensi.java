5fb45d514fc5414db7e33e2433a534ddd065e726
fareldev <ffarelscho@gmail.com>
2025-10-01 07:46:22 +0700

    Tambah file ifCetakKRSNoPresensi.class dan ifCetakKRSNoPresensi.java

---
+++ jobsheet5/ifCetakKRSNoPresensi.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ifCetakKRSNoPresensi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("-- Cetak KRS SIAKAD --");
+        System.out.print("Apakah UKT sudah lunas? (true/false): ");
+        boolean uktLunas = sc.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT sudah lunas.");
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        } else {
+            System.out.println("Pembayaran UKT belum lunas.");
+            System.out.println("Silahkan lunasi UKT terlebih dahulu sebelum mencetak KRS.");
+        }
+    }
+}
